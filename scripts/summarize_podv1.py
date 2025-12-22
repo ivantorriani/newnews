78db9ef4fd23e2de57256f0e4c1b165a84301362
itorriani <sotivantorriani@gmail.com>
2025-12-21 18:39:23 -0800

    feat * added more capabilities to summarizaiton

--- scripts/summarize_podv1.py
+++ scripts/summarize_podv1.py
@@ -6,35 +6,31 @@ Description: Summarize a podcast with ollama. Assumes that the podcast already e
 #Imports---------------------------------
 from ollama import chat 
 from ollama import ChatResponse
+from summrize_podv2 import summarize_pod
 import time
 #Imports---------------------------------
 
-def summarize_pod():
+def organize_sum():
 
     #Read the podcast transcript
     with open("public/episodes/transcripts/" + time.strftime("%Y-%m-%d") + ".txt", 'r') as f:
         transcript = f.read()
+    
+    transcript_summarized = summarize_pod(transcript)
 
     #Get the response
     response: ChatResponse = chat(model='phi3.5:latest', messages=[
         {
             "role": "user",
-            "content": "Your job is to summarize the biggest developments of long news podcast trancripts."
-            "You must pick at most 5 developing stories."
-            "You must organize them according to their place"
-            "Output only in this exact format: \n\n"
-            "New York: 2008 Wall Street Financial Meltdown\n"
-            "London: Brexit Referendum Triggers UK–EU Divorce\n"
-            "Beijing: Rapid Military Buildup Reshapes Asia–Pacific Balance\n"
-            "Kyiv: Russian Invasion Spurs Full-Scale European Security Crisis\n"
-            "Tokyo: Fukushima Nuclear Disaster Sparks Global Energy Reckoning\n"
+            "content": 
+            """
+            You will be given a summarized text blob. Your job is to extract all the important 
+            information and summarize it within 3 sentences.
 
-            "Here is the transcript that you will summarize today: " + transcript
+            Here is the content """ + transcript_summarized 
         }
     ])
 
     print(response['message']['content'])
 
 
-if __name__ == "__main__":
-    summarize_pod()
